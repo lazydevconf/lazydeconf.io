2dd3e30227177fca709c37009560b36b6ab23145
HYEJIN <hjwoou@gmail.com>
2024-01-10 01:17:57 +0900

    [feat] 프로그램 수정 - 뱃지 구분 - 모바일에서는 description 제거 - 칸별 크기 조정

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,6 +1,5 @@
 import { Element } from "react-scroll";
 import img from '../images/time-table-2.jpg'
-import logo from '../images/main-logo.png'
 import header from '../images/time-table-header.jpg'
 import { useInView } from "react-intersection-observer";
 import styled from "styled-components";
@@ -43,17 +42,28 @@ const Section = styled.section`
 }
 
 .badge span {
-    background-color: #fffbec;
-    width: 60px;
     height: 25px;
-    padding-bottom: 3px;
+    padding: 3px 5px;
     border-radius: 5px;
     display: flex;
-    color: #fed85d;
     justify-content: center;
-    align-items: center
+    align-items: center;
 }
 
+.infra {
+  background-color: #ebf5ff;
+  color: #3d6efd;
+}
+
+.overview {
+  background-color: #edf7ed;
+  color: #50c878;
+}
+
+.exp {
+  background-color: #ffe6f5;
+  color: #ff69b4;
+}
 
 .description {
     font-size: 0.9rem;
@@ -75,135 +85,63 @@ export default function Programs() {
   const {ref, inView} = useInView({root:null, rootMargin: "0px", threshold: 0});
   const isMobile = useIsMobile();
 
-  const dbSpeakers = [
-    {icon: 'bx:bxl-mailchimp', category: 'DB/SQL', name: '설동민', time: '[랜] 15:10 ~ 15:40', title: 'jOOQ, SQL을 작성하는 또다른 방법', subTitle: '', description: '자바진영에서 SQL을 작성하며 겪었던 고민과 이를 해소하기 위해 jOOQ를 도입했던 경험을 공유합니다.'},
-    {icon: 'bx:bxl-mailchimp', category: 'DB/SQL', name: '조유신', time: '[랜] 15:50 ~ 16:20', title: 'JPA Patterns', subTitle: '', description: '우리가 작성하는 JPA 코드에는 어떤 디자인 패턴들이 숨어있는지 함께 알아봅니다.'},
-    {icon: 'bx:bxl-mailchimp', category: 'DB/SQL', name: '신수웅', time: '[테] 14:30 ~ 15:00', title: '분산트랜잭션 : 과거와 현재', subTitle: '(기술의 발전 과정과 예시로 알아보는)', description: '분산트랜잭션의 발전 과정을 살펴보면서 현재의 우리는 어떻게 대응할 수 있는가를 알아봅니다.'},
+  const infra = [
+    {icon: 'bx:bxl-mailchimp', category: 'Infra', name: '설동민', time: '[랜] 15:10 ~ 15:40', title: 'jOOQ, SQL을 작성하는 또다른 방법', subTitle: '', description: '자바진영에서 SQL을 작성하며 겪었던 고민과 이를 해소하기 위해 jOOQ를 도입했던 경험을 공유합니다.'},
+    {icon: 'bx:bxl-mailchimp', category: 'Infra', name: '조유신', time: '[랜] 15:50 ~ 16:20', title: 'JPA Patterns', subTitle: '', description: '우리가 작성하는 JPA 코드에는 어떤 디자인 패턴들이 숨어있는지 함께 알아봅니다.'},
+    {icon: 'bx:bxl-mailchimp', category: 'Infra', name: '신수웅', time: '[테] 14:30 ~ 15:00', title: '분산트랜잭션 : 과거와 현재', subTitle: '(기술의 발전 과정과 예시로 알아보는)', description: '분산트랜잭션의 발전 과정을 살펴보면서 현재의 우리는 어떻게 대응할 수 있는가를 알아봅니다.'},
   ]
-  const performanceSpeakrs = [
-    {icon: 'bx:bxl-mailchimp', category: '사례', name: '이한솔', time: '[테] 16:50 ~ 17:20', title: 'Kotlin Injection', subTitle: '(레거시 프로젝트 유지보수팀에 신규 기술 스택 주입시키기)', description: ''},
-    {icon: 'bx:bxl-mailchimp', category: '아키텍처', name: '김우재', time: '[랜] 17:30 ~ 18:00', title: 'JDK Structured Concurrency', subTitle: '', description: 'JDK21 의 Preview 기능인 Structured Concurrency에 대해 알아봅니다.'},
-    {icon: 'bx:bxl-mailchimp', category: '성능', name: '김석현', time: '[테] 15:10 ~ 15:40', title: '소비자 관점의 API 설계 패턴, 사례 훑어보기', subTitle: '', description: ''},
-    {icon: 'bx:bxl-mailchimp', category: '성능', name: '최상용', time: '[테] 15:50 ~ 16:20', title: 'gRPC 입문기', subTitle: '', description: ''},
+  const overview = [
+    {icon: 'bx:bxl-mailchimp', category: 'Overview', name: '이한솔', time: '[테] 16:50 ~ 17:20', title: 'Kotlin Injection', subTitle: '(레거시 프로젝트 유지보수팀에 신규 기술 스택 주입시키기)', description: '새로운 기술 스택을 도입하며 겪게되는 문제들을 어떻게 해결해나가고 같이 성장해나가고 문화를 만들어내는지에 대해 알아봅니다.'},
+    {icon: 'bx:bxl-mailchimp', category: 'Overview', name: '김우재', time: '[랜] 17:30 ~ 18:00', title: 'JDK Structured Concurrency', subTitle: '', description: 'JDK21 의 Preview 기능인 Structured Concurrency에 대해 알아봅니다.'},
+    {icon: 'bx:bxl-mailchimp', category: 'Overview', name: '김석현', time: '[테] 15:10 ~ 15:40', title: '소비자 관점의 API 설계 패턴, 사례 훑어보기', subTitle: '', description: '대규모 서비스 API들이 어떤 변화를 겪었는지 알아보고 소비자와 함께하는 API 설계에 대한 대략적인 개념들을 살펴봅니다.'},
+    {icon: 'bx:bxl-mailchimp', category: 'Overview', name: '최상용', time: '[테] 15:50 ~ 16:20', title: 'gRPC 입문기', subTitle: '', description: 'REST 를 대체할 수 있는 새로운 프로토콜에 대해서 소개합니다.'},
   ]
-  const speakers = [
-    {icon: 'bx:bxl-mailchimp', category: '사례', name: '이상민', time: '[테] 17:30 ~ 18:00', title: '2년차 주니어 개발자의 성능 개선 도전기', subTitle: '', description: ''},
-    {icon: 'bx:bxl-mailchimp', category: '사례', name: '황보성우', time: '[랜] 16:50 ~ 17:20', title: '신규 서비스 개발기', subTitle: '(난 무슨 삽질들을 해왔나)', description: ''},
-    {icon: 'bx:bxl-mailchimp', category: '문화', name: '권승철', time: '[랜] 14:30 ~ 15:00', title: '물경력에 빠진 당신에게 던지는 지푸라기', subTitle: '', description: '경직된 팀 문화를 개선해서 행복한 회사 생활을 꿈꿔봅니다.'},
+  const exp = [
+    {icon: 'bx:bxl-mailchimp', category: 'Experience', name: '이상민', time: '[테] 17:30 ~ 18:00', title: '2년차 주니어 개발자의 성능 개선 도전기', subTitle: '', description: '주니어 개발자가 경험한 레거시 프로젝트에서의 성능 개선 경험 공유합니다.'},
+    {icon: 'bx:bxl-mailchimp', category: 'Experience', name: '황보성우', time: '[랜] 16:50 ~ 17:20', title: '신규 서비스 개발기', subTitle: '(난 무슨 삽질들을 해왔나)', description: '신규 서비스들을 개발하며 느꼈던 잘못했던 점과 이런 잘못을 저지르지 않으면 어떻게 해야할까에 대해 고민했던 부분을 털어놓습니다.'},
+    {icon: 'bx:bxl-mailchimp', category: 'Experience', name: '권승철', time: '[랜] 14:30 ~ 15:00', title: '물경력에 빠진 당신에게 던지는 지푸라기', subTitle: '', description: '경직된 팀 문화를 개선해서 행복한 회사 생활을 꿈꿔봅니다.'},
   ];
+  const alls = {
+    infra,
+    overview,
+    exp
+  }
 
   return (
     <Element name={"Programs"} id="programs">
       <Section className={`${inView ? 'in' : ''}`} ref={ref}>
-      <Container className="mb-3">
+      <Container className="my-3">
           <div className="mb-5" role="button" onClick={() => window.open(img, '_blank', 'noopener')}>
             <img src={header} className="w-100"/>           
           </div>
-        <Row>
-        <Col md={1} className="d-flex flex-col justify-content-center align-items-center text-center ">
-          <h4><b>DB/SQL</b><br/> {isMobile ? '':<Icon className="shake" icon="maki:arrow" />}</h4>
-        </Col>
-        {
-          dbSpeakers.map((speaker) => (
-            <Col className="mb-3">
-                <Card className="p-3">
-                  <div className="d-flex justify-content-between">
-                    <div className="d-flex flex-row align-items-center">
-                      <div className="icon"><Icon icon={speaker.icon} /></div>
-                      <div className="ms-2 c-details">
-                        <h6 className="mb-0">{speaker.name}</h6>
-                        <span>{speaker.time}</span>
-                      </div>
-                    </div>
-                    <div className="badge"><span>{speaker.category}</span></div>
-                  </div>
-                  <div className="mt-5">
-                    <h3>{speaker.title}</h3>
-                    <h6>{speaker.subTitle}</h6>
-                    <div className="mt-5">
-                      <div className="mt-3">
-                        <span className="description">{speaker.description}</span>
-                      </div>
+        
+          {Object.entries(alls).map(([key, value]) => (
+            <Row>
+            {value.map((speaker) => (
+              <Col lg={3} className="mb-3">
+              <Card className="p-3">
+                <div className="d-flex justify-content-between">
+                  <div className="d-flex flex-row align-items-center">
+                    <div className="icon"><Icon icon={speaker.icon} /></div>
+                    <div className="ms-2 c-details">
+                      <h6 className="mb-0">{speaker.name}</h6>
+                      <span>{speaker.time}</span>
                     </div>
                   </div>
-                </Card>
-              </Col>
-              ))
-            }
-      </Row>
-      <Row>
-      <Col md={1} className="d-flex flex-col justify-content-center align-items-center text-center">
-      <h4><b>기술</b><br/> {isMobile ? '':<Icon className="shake" icon="maki:arrow" />}</h4>
-        </Col>
-        {
-          performanceSpeakrs.map((speaker) => (
-            <Col className="mb-3">
-                <Card className="p-3">
-                  <div className="d-flex justify-content-between">
-                    <div className="d-flex flex-row align-items-center">
-                      <div className="icon"><Icon icon={speaker.icon} /></div>
-                      <div className="ms-2 c-details">
-                        <h6 className="mb-0">{speaker.name}</h6>
-                        <span>{speaker.time}</span>
-                      </div>
+                  <div className="badge"><span className={`${key}`}>{speaker.category}</span></div>
+                </div>
+                <div className="mt-5 h-100 d-flex flex-column">
+                  <h3 className="flex-grow-1">{speaker.title}</h3>
+                    <div className={`mt-3 ${isMobile ? 'd-none' : ''}`}>
+                      <span className="description">{speaker.description}</span>
                     </div>
-                    <div className="badge"><span>{speaker.category}</span></div>
-                  </div>
-                  <div className="mt-5">
-                    <h3>{speaker.title}</h3>
-                    <h6>{speaker.subTitle}</h6>
-                    <div className="mt-5">
-                      <div className="mt-3">
-                        <span className="description">{speaker.description}</span>
-                      </div>
-                    </div>
-                  </div>
-                </Card>
-              </Col>
-              ))
-            }
-      </Row>
-      <Row>
-      <Col md={1} className="d-flex flex-col justify-content-center align-items-center text-center">
-      <h4><b>경험</b><br/> {isMobile ? '':<Icon className="shake" icon="maki:arrow" />}</h4>
-        </Col>
-        {
-          speakers.map((speaker) => (
-            <Col  className="mb-3">
-                <Card className="p-3">
-                  <div className="d-flex justify-content-between">
-                    <div className="d-flex flex-row align-items-center">
-                      <div className="icon"><Icon icon={speaker.icon} /></div>
-                      <div className="ms-2 c-details">
-                        <h6 className="mb-0">{speaker.name}</h6>
-                        <span>{speaker.time}</span>
-                      </div>
-                    </div>
-                    <div className="badge"><span>{speaker.category}</span></div>
-                  </div>
-                  <div className="mt-5">
-                    <h3>{speaker.title}</h3>
-                    <h6>{speaker.subTitle}</h6>
-                    <div className="mt-5">
-                      <div className="mt-3">
-                        <span className="description">{speaker.description}</span>
-                      </div>
-                    </div>
-                  </div>
-                </Card>
-              </Col>
-              ))
-            }
-      </Row>
-    </Container>
-    {/*        <Col md={3} className="mb-3">
-        <Card className="p-3 justify-content-center align-items-center gap-5" role="button" onClick={() => window.open(img, '_blank', 'noopener')}>
-          <img src={logo} className="w-50" />
-          <span>타임테이블 보기</span>
-          </Card>
-       </Col>
- */}
-        {/* <img src={img} style={{cursor: 'pointer'}} onClick={() => window.open(img, '_blank', 'noopener')}/> */}
+                </div>
+              </Card>
+            </Col>
+            ))}
+            </Row>
+          ))}
+      </Container>
       </Section>
     </Element>
   );
